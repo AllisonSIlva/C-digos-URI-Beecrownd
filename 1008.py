a6dc98ab2bc1a088e1e34ee34ddc26597b3a7270
AllisonSIlva <74565574+AllisonSIlva@users.noreply.github.com>
2022-03-20 23:19:31 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+funcionario = int(input())
+
+horas = int(input())
+
+valor = float(input())
+
+salario = horas * valor
+
+print("NUMBER =" , funcionario)
+print("SALARY = U$ {:.2f}".format(salario))
